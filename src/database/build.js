cf23dc618c4c34bf8368b816c3bb178e0e64ee2b
Amber <a.rignell16@ejm.org>
2020-08-13 15:34:25 +0100

    Function to add form inputs to database Terrence Chung <netceer112@gmail.com>

--- src/database/build.js
+++ src/database/build.js
@@ -1,24 +1,25 @@
+/** @format */
+
 const fs = require("fs");
 const path = require("path");
-const db = require("./connection")
+const db = require("./connection");
 
 const initPath = path.join(__dirname, "init.sql");
 const initSQL = fs.readFileSync(initPath, "utf-8", (error, file) => {
-    if (error) {
-        response.writeHead(500, { "content-type": "text/html" });
-        response.end(`<h1>Server error</h1>`);
-      } else {
-        console.log('init file read')
-      }
+	if (error) {
+		response.writeHead(500, { "content-type": "text/html" });
+		response.end(`<h1>Server error</h1>`);
+	} else {
+		console.log("init file read");
+	}
 });
 
-
 function build() {
-    return db.query(initSQL);
+	return db.query(initSQL);
 }
 
 if (require.main === module) {
-    build().then(() => db.end());
+	build().then(() => db.end());
 }
 
-module.exports = build; 
+module.exports = build;
