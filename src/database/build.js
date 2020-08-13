a1e4d5066404920f78d0de7c9a303d1b54810e0d
Akshaya <akshayasatheesh@hotmail.co.uk>
2020-08-13 15:00:13 +0100

    Fixed some bugs and also created first database test Relates #5
    Co-authored-by: Trish <trishhasone@gmail.com>

--- src/database/build.js
+++ src/database/build.js
@@ -3,15 +3,16 @@ const path = require("path");
 const db = require("./connection")
 
 const initPath = path.join(__dirname, "init.sql");
-const initSQL = fs.readFileSync(initPath, "utf-8", (error, file) => {
-    if (error) {
-        response.writeHead(500, { "content-type": "text/html" });
-        response.end(`<h1>Server error</h1>`);
-      } else {
-        console.log('init file read')
-      }
-});
+const initSQL = fs.readFileSync(initPath, "utf-8")
 
+// (error, file) => {
+//     if (error) {
+//         response.writeHead(500, { "content-type": "text/html" });
+//         response.end(`<h1>Server error</h1>`);
+//       } else {
+//         console.log('init file read')
+//       }
+// });
 
 function build() {
     return db.query(initSQL);
