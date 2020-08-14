44ae68e60fe2d0074b965e3e79e54f1b6b8740d1
Amber <a.rignell16@ejm.org>
2020-08-14 14:28:53 +0100

    remove merge marks in build file

--- src/database/build.js
+++ src/database/build.js
@@ -5,7 +5,6 @@ const path = require("path");
 const db = require("./connection");
 
 const initPath = path.join(__dirname, "init.sql");
-<<<<<<< HEAD
 const initSQL = fs.readFileSync(initPath, "utf-8", (error, file) => {
 	if (error) {
 		response.writeHead(500, { "content-type": "text/html" });
@@ -15,19 +14,6 @@ const initSQL = fs.readFileSync(initPath, "utf-8", (error, file) => {
 	}
 });
 
-=======
-const initSQL = fs.readFileSync(initPath, "utf-8")
-
-// (error, file) => {
-//     if (error) {
-//         response.writeHead(500, { "content-type": "text/html" });
-//         response.end(`<h1>Server error</h1>`);
-//       } else {
-//         console.log('init file read')
-//       }
-// });
-
->>>>>>> master
 function build() {
 	return db.query(initSQL);
 }
